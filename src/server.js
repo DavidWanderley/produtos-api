10321dd7ef9f71f2e43bf6c7c3301f0da0065e75
David Wanderley <davidgomeswanderley@gmail.com>
2025-12-02 19:46:58 -0300

    atualizando a api

---
+++ src/server.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import { sequelize } from './config/database.js';
+
+const HOST = '127.0.0.1'
+const PORT = '5000'
+
+const app = express();
+
+// Middlewares
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('API de Produtos funcionando 🚀');
+})
+
+
+try {
+    await sequelize.authenticate()
+    console.log('🟢 Conectado ao banco de dados!');
+} catch (error) {
+    console.error('🔴 Erro ao conectar no banco:', error);
+}
+
+//Iniciando o server
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://${HOST}:${PORT}`);
+})
+
+
+async function start() {
+  try {
+    await sequelize.authenticate();
+    console.log("🎉 Conectado ao Postgres Neon com sucesso!");
+
+    await sequelize.sync();
+    console.log("📦 Modelos sincronizados com o banco!");
+
+    app.listen(process.env.PORT, () =>
+      console.log(`🚀 Servidor rodando na porta http://${HOST}:${PORT}`)
+    );
+  } catch (err) {
+    console.error("Erro ao iniciar o servidor:", err);
+  }
+}
+
+start();
